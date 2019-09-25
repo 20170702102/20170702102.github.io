7016165d98eab044317b2c2c200832ad16a99f81
1548965360 <1548965360@qq.com>
2019-09-25 13:30:21 +0800

    Site updated: 2019-09-25 13:30:20

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -1,8 +1,7 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        var a = new Array
-        ("敏敏");
+        var a = new Array("min");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
