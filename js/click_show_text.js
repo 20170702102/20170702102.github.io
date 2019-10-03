8b429c1d3a3c0d6dd946850215396de05eee4c34
1548965360 <1548965360@qq.com>
2019-10-03 13:46:15 +0800

    Site updated: 2019-10-03 13:46:13

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -1,7 +1,7 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        var a = new Array("min","敏","song","宋");
+        var a = new Array("min");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
